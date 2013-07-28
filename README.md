773669763ced5bd0868fc869f17c8e61b27c7ef2
congdat <congdat@congdat-K53E.(none)>
2013-07-28 15:45:32 +0700

    First commit

--- README.md
+++
@@ -1,5 +0,0 @@
-vss2013-02_online-movie-web
-===========================
-
-VSS 2013 - Team 2 - Online Movie Web (Thanh Toan Nguyen - Mai Công Đạt).
-Using Rails on Ruby for creating our website.
---
+++ movie_web/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovieWeb::Application.config.secret_key_base = 'f70444d48842aa13419f4741427dd402bc867c8ba2409fdbcb55c78a0bc2f31cea247ada7afe9b67011de7e9a9ceabe93a63a9b6c6f14dd1f13a02a6fceb915b'
